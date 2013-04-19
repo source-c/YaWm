1dc43ad6ecbc8971ac6562b4cef7bc8a709a32fb
MelKori <mieldo@gmail.com>
2013-04-19 13:27:19 +0300

    a bit polished user configuration

--- Makefile
+++ Makefile
@@ -6,5 +6,5 @@ all:
 	@for dir in $(DIRS); do $(MAKE) -C $$dir all; done
 clean:
 	 @for dir in $(DIRS); do $(MAKE) -C $$dir clean; done
-clean-all:
+clean-all: clean
 	 @for dir in $(DIRS); do $(MAKE) -C $$dir clean-all; done
--- wm/config.def.h
+++ wm/config.def.h
@@ -28,11 +28,11 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",      NULL,       NULL,       1 << 8,       True,        -1 },
 	{ "opera",     NULL,       NULL,       1 << 1,       True,        -1 },
 	{ "Opera",     NULL,       NULL,       1 << 1,       True,        -1 },
 	{ "luakit",    NULL,       NULL,       1 << 1,       True,        -1 },
 	{ "fbpanel",   NULL,       NULL,       ~0,           True,        -1 },
+	{ "osmo",      NULL,       NULL,       0,            True,        -1 },
 };
 
 /* layout(s) */
